464f5b53a16eaab3ae0072ae203b0df251b00f93
KOWX712 <leecc0503@gmail.com>
2025-06-10 20:37:45 +0800

    add action.sh for Magisk
    refs: https://github.com/bindhosts/bindhosts/commit/b92dc6898778bb5f2661b1c2fcef3a693ee1b31c
    Co-Authored-By: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -13,6 +13,13 @@ else
     fi
 fi
 
+# Hide action.sh if not using Magisk
+if [ "$KSU" = true ] || [ "$APATCH" = true ]; then
+    [ -f "$MODPATH/action.sh" ] && mv -f "$MODPATH/action.sh" "$MODPATH/action.sh.old"
+else
+    [ -f "$MODPATH/action.sh.old" ] && mv -f "$MODPATH/action.sh.old" "$MODPATH/action.sh"
+fi
+
 # Conditional early sensitive properties
 
 # Samsung
